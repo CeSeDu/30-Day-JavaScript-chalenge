3055debf654e56d423773a306b2c3bc07b76f6df
CeSeDu <senad8127@gmail.com>
2022-04-13 02:27:29 +0300

    firstCommit

---
+++ variable.js
@@ -0,0 +1,15 @@
+/*dize, boolean, tanımsız ve null veri türleri atayın;
+string, boolean, undefined and null data types
+
+string    = veriyi yazı olarak tutar.
+boolean   = true ve false değer tiplerini tutan değişken tipi
+undefined = tanımsız veri içeren değişken tipi
+null      = boş veri tipini tutan değişken.*/
+
+let firstName = "Sena"
+let age       = 19
+let isMarried = true
+let dadName   = " "
+
+console.log(firstName, age,isMarried,dadName)
+
---
+++ yorumSatırı.js
@@ -0,0 +1,3 @@
+//merhaba tek satırlık açıklama kodu:)
+//30DaysOfJavaScript'e Hoş Geldiniz
+/*bu satır kodları daha okunabilir hale getirir ve çok satırlı not tutmak için kullanılır. */
