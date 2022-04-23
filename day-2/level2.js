34d7f402988ed5f64a9001e5125517b570b02097
CeSeDu <senad8127@gmail.com>
2022-04-23 18:03:26 +0300

    yarım:(

---
+++ day-2/level2.js
@@ -0,0 +1,30 @@
+let alinti = "The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another."
+
+console.log(alinti)
+
+const alinti2 ="Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
+
+console.log(alinti2)
+
+
+console.log(parseInt('10' ) === 10) 
+
+let num = '9.81'
+let numFloat = parseFloat(num) //??? yuvarlamadı ???
+console.log(numFloat)
+
+let on = 'python , jargon'
+console.log(on.includes('on'))
+
+const number = Math.floor(Math.random()*101)
+console.log(number)
+
+console.log(Math.floor(Math.random() * -49)+100)
+
+console.log(Math.floor(Math.random() * 256))
+
+let lang ='JavaScript'
+
+// 3 EGZERSİZ DAHA VARrr
+
+
